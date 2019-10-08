a2fdb65f829f03a2d3d5c5ccea0df02bb3537e77
Oeoes Roy <oeoesroy@gmail.com>
2019-10-08 15:47:08 +0700

    base table

--- app/TestReport.php
+++ app/TestReport.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class TestReport extends Model
 {
+    public $table = 'test_requests';
     protected $fillable = ['produk', 'tag', 'parameter', 'metode', 'unit', 'master', 'limit_min', 'limit_max', 'result', 'user_email', 'user_id'];
 }
